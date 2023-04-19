9486e95539b0b1024ea9b670ea489c728ac0fc95
LopezChrisBong <chrisbong.lopez@dnsc.edu.ph>
2023-04-19 11:04:40 +0800

    second  commit

--- mail.php
+++ mail.php
@@ -1,12 +1,20 @@
 <?php
 //get data from form  
-$name = $_POST['name'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$phone = $_POST['phone'];
 $email= $_POST['email'];
+$country = $_POST['country'];
+$subject = $_POST['subject'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+
+
+$to = "abdon.penalis54@gmail.com";
+$txt ="Phone: " . $phone .
+"\r\nFirstname: " . $fname . " " . $lname .
+"\r\nEmail: " . $email . "\r\nCountry: " . $country .
+"\r\nMessage: " . $message;
+$headers = "From: BongWebsite@message.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
